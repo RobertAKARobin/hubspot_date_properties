4126d02892369fed568333906f2cf6296d6a4810
RobertAKARobin <robertgfthomas@gmail.com>
2017-10-10 15:08:44 -0500

    Loops through and prints dates

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-var request = require('request');
+const request = require('request');
+const ENV = require('./env.json');
 
-console.log('hello');
+Date.prototype.getMonthWithZeroes = function(){
+	let date = this;
+	return ('0' + (date.getMonth()+1)).slice(-2);
+}
+
+let startDate = new Date(2012, 06);
+goToNextDate();
+
+function goToNextDate(){
+	let currentDate = startDate.getFullYear() + '_' + startDate.getMonthWithZeroes();
+	startDate.setMonth(startDate.getMonth() + 1);
+	console.log(currentDate);
+	if(currentDate != '2022_12'){
+		goToNextDate();
+	}
+}

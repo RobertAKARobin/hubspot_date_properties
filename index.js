d52368498fdd9f73d4fa1b857b417c719ee704d4
RobertAKARobin <robertgfthomas@gmail.com>
2017-10-11 14:42:06 -0500

    Added destroy

--- index.js
+++ index.js
@@ -6,39 +6,67 @@ Date.prototype.getMonthWithZeroes = function(){
 	return ('0' + (date.getMonth()+1)).slice(-2);
 }
 
-let startDate = new Date(2012, 06);
+let startDate = new Date(2013, 03);
 goToNextDate();
 
 function goToNextDate(){
 	let year = startDate.getFullYear();
 	let month = startDate.getMonthWithZeroes();
-	startDate.setMonth(parseInt(month) + 1);
+	let internalName = ['revenue', year, month].join('_');
+	let displayName = 'Revenue ' + year + '/' + month;
+	console.log(internalName);
 
-	request({
-		method: 'POST',
-		url: 'https://api.hubapi.com/properties/v1/deals/properties?hapikey=' + ENV.HAPIKEY,
-		headers: {
-			'Content-Type': 'application/json'
-		},
-		body: JSON.stringify({
-			name: ['revenue', year, month].join('_'),
-			label: 'Revenue ' + year + '/' + month,
-			groupName: 'revenue_by_month',
-			type: 'number',
-			fieldType: 'number',
-			showCurrencySymbol: true
-		})
-	}, function(error, response, body){
-		var apiResponse = JSON.parse(body)
-		console.log(apiResponse);
-		if(apiResponse.status == 'error'){
-			return;
-		}else{
-			if(year == '2012' && month == '07'){
+	startDate.setMonth(startDate.getMonth() + 1);
+
+	create();
+	// destroy();
+
+	function destroy(){
+		request({
+			method: 'DELETE',
+			url: 'https://api.hubapi.com/properties/v1/deals/properties/named/' + internalName + '?hapikey=' + ENV.HAPIKEY,
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		}, function(error, response, body){
+			console.log(error || response.statusCode);
+			if(error){
 				return;
 			}else{
 				goToNextDate();
 			}
-		}
-	});
+		})
+	}
+
+	function create(){
+		request({
+			method: 'POST',
+			url: 'https://api.hubapi.com/properties/v1/deals/properties?hapikey=' + ENV.HAPIKEY,
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				name: internalName,
+				label: displayName,
+				groupName: 'revenue_by_month',
+				type: 'number',
+				fieldType: 'number',
+				showCurrencySymbol: true
+			})
+		}, function(error, response, body){
+			var apiResponse = JSON.parse(body)
+			console.log(apiResponse);
+			if(apiResponse.status == 'error'){
+				return;
+			}else{
+				if(year == '2022' && month == '12'){
+					return;
+				}else{
+					goToNextDate();
+				}
+			}
+		})
+	}
+
+
 }

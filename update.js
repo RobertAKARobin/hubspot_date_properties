341d881c16e917f6500a87a54349f830358f18a6
RobertAKARobin <robertgfthomas@gmail.com>
2017-10-19 16:24:34 -0500

    Elimintaed need for Excel

--- update.js
+++ update.js
@@ -2,55 +2,129 @@ const request = require('request');
 const lineReader = require('line-reader');
 const ENV = require('./env.json');
 
-let dealPropertyNames = [];
-let dealCount = -1;
-let dealsToUpload = [];
-let dealsPerAPIUpload = 100;
-
-let requestParams = {
-	method: 'POST',
-	url: 'https://api.hubapi.com/deals/v1/batch-async/update?hapikey=' + ENV.HAPIKEY,
-	headers: {
-		'Content-Type': 'application/json'
-	},
-	body: null
+Date.prototype.getMonthWithZeroes = function(){
+	var date = this;
+	return ('0' + (date.getMonth()+1)).slice(-2);
+}
+Date.prototype.getDateWithZeroes = function(){
+	var date = this;
+	return ('0' + date.getDate()).slice(-2);
+}
+Date.prototype.toArray = function(){
+	var date = this;
+	return [date.getFullYear(), date.getMonthWithZeroes(), date.getDateWithZeroes()];
 }
 
-lineReader.eachLine('./data.csv', function(csvLine, isLast, next){
-	dealCount += 1;
-	csvLine = csvLine.split(',');
+var sfHeaders = {
+	date: 1,
+	amount: 2,
+	sfId: 5
+}
+var hsHeaders = {
+	hsId: 0,
+	sfId: 1
+}
+var sfFile = './sfdata.csv';
+var hsFile = './dealid_with_oppid.tsv';
 
-	if(dealCount === 0){
-		dealPropertyNames = csvLine.slice(1);
-		next();
-	}else{
-		let deal = {
-			'objectId': csvLine.shift(),
-			'properties': []
-		};
-		for(let i = 0; i < dealPropertyNames.length; i++){
-			deal['properties'].push({
-				name: dealPropertyNames[i],
-				value: csvLine[i]
+var deals = {};
+
+mapHSData();
+
+function mapHSData(){
+	var count = -1;
+	lineReader.eachLine(hsFile, function(tsvLine, isLast, next){
+		tsvLine = tsvLine.split('\t');
+		count += 1;
+
+		if(count > 0){
+			var hsId = tsvLine[hsHeaders.hsId];
+			var sfId = tsvLine[hsHeaders.sfId];
+			deals[sfId] = (deals[sfId] || {
+				sfId: sfId,
+				hsId: hsId,
+				months: {}
 			});
 		}
-		dealsToUpload.push(deal);
-
-		if(dealsToUpload.length == dealsPerAPIUpload || isLast){
-			requestParams.body = JSON.stringify(dealsToUpload);
-			console.log(dealCount + ' ' + dealsToUpload.length);
-			request(requestParams, function(error, response, body){
-				if(error){
-					console.log(error);
-					next(false);
-				}else{
-					console.log(body);
-					dealsToUpload = [];
-					next();
-				}
-			});
+		if(isLast){
+			mapSFData();
 		}else{
 			next();
 		}
+	});
+}
+
+function mapSFData(){
+	var count = -1;
+	lineReader.eachLine(sfFile, function(csvLine, isLast, next){
+		csvLine = csvLine.split(',');
+		count += 1;
+		
+		if(count > 0){
+			var schedule = {
+				sfId: csvLine[sfHeaders.sfId],
+				date: (new Date(csvLine[sfHeaders.date])).toArray(),
+				amount: parseFloat(csvLine[sfHeaders.amount])
+			}	
+			var monthName = ['revenue', schedule.date[0], schedule.date[1]].join('_');
+			var deal = deals[schedule.sfId];
+			if(deal){
+				deal.months[monthName] = (deal.months[monthName] || 0) + schedule.amount;
+			}
+		}
+		if(isLast){
+			prepareHSData();
+		}else{
+			next();
+		}
+	});
+}
+
+var hsData = [];
+function prepareHSData(){
+	Object.values(deals).forEach(function(deal){
+		var datum = {
+			objectId: deal.hsId,
+			properties: []
+		};
+		Object.keys(deal.months).forEach(function(monthName){
+			datum.properties.push({
+				name: monthName,
+				value: deal.months[monthName]
+			})
+		});
+		hsData.push(datum);
+	});
+
+	uploadHSData();
+}
+
+function uploadHSData(){
+	var index = 0;
+	console.log(hsData.length)
+	sendNextBatch();
+
+	function sendNextBatch(){
+		if(index < hsData.length){
+			var dealBatch = hsData.slice(index, index + 100);
+			console.log(index)
+			console.log(dealBatch.length);
+			request({
+				method: 'POST',
+				url: 'https://api.hubapi.com/deals/v1/batch-async/update',
+				qs: {
+					hapikey: ENV.HAPIKEY
+				},
+				body: JSON.stringify(dealBatch)
+			}, function(error, response, body){
+				console.log(response.statusCode)
+				if(error){
+					console.log(body);
+				}else{
+					index += 100;
+					sendNextBatch();
+				}
+			});
+		}
 	}
-});
+}
